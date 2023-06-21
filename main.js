9ef46d6fea6fb5a8cbe256a831dc6dcfb609a3e7
Shrihan2 <90690673+Shrihan2@users.noreply.github.com>
2023-06-21 16:38:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ video="";
 status="";
 objects=[];
 function preload(){
-    
+    video=createCapture(VIDEO);
     video.hide();
 }
 function setup(){
@@ -15,16 +15,16 @@ function draw(){
         objectDetector.detect(video,gotResult);
         for(i=0;i<objects.length;i++){
             document.getElementById("status").innerHTML="Status: Objects Detected";
-            if(objects=input){
-                video.stop();
-                document.getElementById("is_found").innerHTML="object mantioned is found";
-            }
             fill("#FF0000");
             percent=floor(objects[i].confidence*100);
             text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
             noFill();
             stroke("#FF0000");
             rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(objects=input){
+                video.stop();
+                document.getElementById("is_found").innerHTML="object mentioned is found";
+            }
         }
     }
 }
